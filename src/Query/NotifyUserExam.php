8548f133df89defc3390501d5bf77d857528b476
shola129 <ekundayoshola129@gmail.com>
2026-03-31 22:33:15 +0100

    1.2.0.6vs

--- src/Query/NotifyUserExam.php
+++ src/Query/NotifyUserExam.php
@@ -7,7 +7,25 @@ use NewdichSchema\Migration;
 
 class NotifyUserExam{
     private $dto;
-    private $table = 
+    // private $table = Platform::SETEXAMTIME_TABLE;
+    private $table2 = Platform::USERS_TABLE;
+
+    public function __construct(AnsofraDto $dto){
+        $this->dto=$dto;
+    }
+
+    public function process(){
+        $where = [
+            "department"=>$this->dto->department
+        ];
+
+        $newMig = new Migration(null, $this->table2);
+        $mig = $newMig->get($where, 0, 20);
+        $decodeMig = json_decode($mig, true);
+        if($decodeMig['status']==="success"){
+            $reponse = $decodeMig['reponse'];
+        }
+    }
 }
 
 ?>
