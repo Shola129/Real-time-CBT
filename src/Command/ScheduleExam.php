0ff37ed2ef2e33cbdde16c69bd89e4ca6ea5630e
shola129 <ekundayoshola129@gmail.com>
2026-04-02 15:01:25 +0100

    2.9.1.2vs

--- src/Command/ScheduleExam.php
+++ src/Command/ScheduleExam.php
@@ -25,63 +25,63 @@ class ScheduleExam{
         $mig = $newMig->get($where, 0, 1);
         $decodeMig = json_decode($mig, true);
         if($decodeMig['status']==="success"){
-        //     $col = "timeID";
-        //     $val = $this->dto->department . $this->dto->DepartmentCode;
-        //     $data = [
-        //         "department"=> $this->dto->department,
-        //         'departmentCode'=> $this->dto->departmentCode,
-        //         'date'=> $this->dto->date,
-        //         'start'=> $this->dto->start,
-        //         'end'=> $this->dto->end,
-        //         'duration'=> $this->dto->duration,
-        //         'timeID'=> $this->dto->department . $this->dto->DepartmentCode,
-        //         'role'=> 'set'
-        //     ];
+            $col = "timeID";
+            $val = $this->dto->department . $this->dto->DepartmentCode;
+            $data = [
+                "department"=> $this->dto->department,
+                'departmentCode'=> $this->dto->departmentCode,
+                'date'=> $this->dto->date,
+                'start'=> $this->dto->start,
+                'end'=> $this->dto->end,
+                'duration'=> $this->dto->duration,
+                'timeID'=> $this->dto->department . $this->dto->DepartmentCode,
+                'role'=> 'set'
+            ];
 
-        //     $newMig2 = new Migration(null, $this->table);
-        //     $mig2 = $newMig2->saveUnique($col, $val, $data);
-        //     $decodeMig2 = json_decode($mig2, true);
-        //     if($decodeMig2['status']==="success"){
-        //         $getAllUserInThatDep = [
-        //             "department"=>$this->dto->department
-        //         ];
+            $newMig2 = new Migration(null, $this->table);
+            $mig2 = $newMig2->saveUnique($col, $val, $data);
+            $decodeMig2 = json_decode($mig2, true);
+            if($decodeMig2['status']==="success"){
+                $getAllUserInThatDep = [
+                    "department"=>$this->dto->department
+                ];
                 
-        //         $newMig3 = new Migration(null, $this->table3);
-        //         $mig3 = $newMig3->get($getAllUserInThatDep, 0, 50);
-        //         $decodeMig3 = json_decode($mig3, true);
-        //         if($decodeMig3["status"]==="success"){
-        //             $time = $this->dto->timeSchedule;
-        //             $response = $decodeMig3["response"];
-        //             $allEmail = $response["email"];
-        //             $a = count($allEmail);
-        //             $holdEachEmail='';
-        //             for($i=0; $i<$a; $i++){
-        //                 $holdEachEmail=$allEmail[$i];
-        //             }
-        //             $body = "Th time set for cbt exam is $time";
-        //             $newMail = new Index();
-        //             $mail = $newMail->sendOtp('TimeTable', $body, $holdEachEmail);
-        //             $decodeMail = json_decode($mail, true);
-        //             if($decodeMail['status']==="success"){
-        //                 return json_encode([
-        //                     'status'=>'success',
-        //                     'response'=>'time scheduled have being saved and sent to the registered user department to ready'
-        //                 ], JSON_PRETTY_PRINT);
-        //             }
-        //             else{
-        //                 return $mail;
-        //             }
-        //         }
-        //         else{
-        //             return json_decode([
-        //                 'status'=>'fail',
-        //                 'response'=>'no user found registered for the department'
-        //             ], JSON_PRETTY_PRINT);
-        //         }
-        //     }
-        //     else{
-        //         return $mig2;
-        //     }
+                $newMig3 = new Migration(null, $this->table3);
+                $mig3 = $newMig3->get($getAllUserInThatDep, 0, 50);
+                $decodeMig3 = json_decode($mig3, true);
+                if($decodeMig3["status"]==="success"){
+                    $time = $this->dto->timeSchedule;
+                    $response = $decodeMig3["response"];
+                    $allEmail = $response["email"];
+                    $a = count($allEmail);
+                    $holdEachEmail='';
+                    for($i=0; $i<$a; $i++){
+                        $holdEachEmail=$allEmail[$i];
+                    }
+                    $body = "Th time set for cbt exam is $time";
+                    $newMail = new Index();
+                    $mail = $newMail->sendOtp('TimeTable', $body, $holdEachEmail);
+                    $decodeMail = json_decode($mail, true);
+                    if($decodeMail['status']==="success"){
+                        return json_encode([
+                            'status'=>'success',
+                            'response'=>'time scheduled have being saved and sent to the registered user department to ready'
+                        ], JSON_PRETTY_PRINT);
+                    }
+                    else{
+                        return $mail;
+                    }
+                }
+                else{
+                    return json_decode([
+                        'status'=>'fail',
+                        'response'=>'no user found registered for the department'
+                    ], JSON_PRETTY_PRINT);
+                }
+            }
+            else{
+                return $mig2;
+            }
         }
         else{
             return $mig;
