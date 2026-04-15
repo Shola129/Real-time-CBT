3cbe779e7b503000767771c33cc921c185b0bee9
shola129 <ekundayoshola129@gmail.com>
2026-04-15 15:41:18 +0100

    40.0.0.8vs

--- app/Query/Login.php
+++ app/Query/Login.php
@@ -8,51 +8,57 @@ use NewdichAuth\Authentication;
 
 class Login{
     private $dto;
+    private $mid;
     private $table = Platform::USERS_TABLE;
 
-    public function __construct(AnsofraDto $dto){
+    public function __construct(AnsofraDto $dto, Index $mid){
         $this->dto=$dto;
+        $this->mid=$mid;
     }
 
     public function process(){
         $where = [
-            'email'=>$this->dto->email
+            'email'=>$this->mid->cleanData($this->dto->email),
+            'regNum'=>$this->mid->cleanData($this->dto->regNum)
         ];
 
-        $newMig = new Migration(null, $this->table);
-        $mig = $newMig->get($where, 0,1);
+        $newMIg = new Migration(null, $this->table);
+        $mig = $newMIg->get($where, 0, 1);
         $decodeMig = json_decode($mig, true);
-        if($decodeMig["status"]==="success"){
-            $response = $decodeMig["response"][0];
-            $password = $response["password"];
-            $pass = $this->dto->password;
-            if($pass===$password){
-                $newAuth = new Authentication();
-                $auth = $newAuth->auth($this->dto->email, "USER");
-                $decodeAuth = json_decode($auth, true);
-                if($decodeAuth["status"]==="success"){
-                    return json_encode([
-                        'status'=>'success',
-                        'response'=>'allow in'
-                    ], JSON_PRETTY_PRINT);
-                }
-                else{
-                    return $auth;
-                }
-            }
-            else{
-                return json_encode([
-                    'status'=>'fail',
-                    'response'=>'password not correct'
-                ], JSON_PRETTY_PRINT);
-            }
+        if($decodeMig['status']==='success'){
+            return $mig;
+            // $response = $decodeMig['response'][0];
+            // $password = $response['password'];
+            // $pwd = $this->mid->cleanData($this->dto->password);
+            // if($pwd === $password){
+                $email = $this->mid->cleanData($this->dto->email);
+                $role = "role";
+                // $newAuth  = new Authentication();
+                // $auth = $newAuth->auth($email, $role);
+                // return $auth;
+                // $decodeAuth = json_decode($auth, true);
+                // if($decodeAuth['status']==='success'){
+                //     $res = $decodeAuth['response'];
+                //     $response["response"] = $res;
+                //     return json_encode([
+                //         'status'=>'success',
+                //         'response'=>$response
+                //     ], JSON_PRETTY_PRINT);
+                // }
+                // else{
+                //     return $auth;
+                // }
+            // }
+            // else{
+                // return json_encode([
+                //     'status'=>'failed',
+                //     'response'=>'incorrect password'
+                // ], JSON_PRETTY_PRINT);
+            // }
         }
         else{
-             return json_encode([
-                'status'=>'failed',
-                'response'=>'email no found'
-            ], JSON_PRETTY_PRINT);
-        }
+            return $mig;
+        } 
     }
 }
 
