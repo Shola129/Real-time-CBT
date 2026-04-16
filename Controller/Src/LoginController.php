3b1aaa1ce6aec5f9a133e59ea0fbc2088c912f17
shola129 <ekundayoshola129@gmail.com>
2026-04-16 14:42:48 +0100

    5.0.0.0.6vs

--- Controller/Src/LoginController.php
+++ Controller/Src/LoginController.php
@@ -6,10 +6,46 @@ use NewdichSrc\Query\Login;
 
 $data = $_POST;
 $mid = new Index();
-$dto = new AnsofraDto($data);
-$logic = new Login($dto, $mid);
+$cleanData = [];
+foreach($data as $key=>$val){
+    if($key==="password"){
+        $cleanData["key"]=$mid->hashData($val);
+    }
+    else{
+        $cleanData["key"]=$mid->cleanData($val);
+    }
+}
+
+$dto = new AnsofraDto($cleanData);
+$logic = new Login($dto);
 $log = $logic->process();
-echo $log;
+$decode = json_decode($log, true);
+if($decode["status"]==="success"){
+    $response = $decode["response"][0];
+    $pass  = $response["password"];
+    $password = $cleanData["password"];
+    if($pass === $password){
+        echo json_encode([
+            'status'=>'success',
+            'response'=>'verification successful'
+        ], JSON_PRETTY_PRINT);
+        exit();
+    }
+    else{
+        echo  json_encode([
+            'status'=>'failed',
+            'response'=>'incorrect password'
+        ], JSON_PRETTY_PRINT);
+        exit();
+    }
+}
+else{
+    echo json_encode([
+        'status'=>'failed',
+        'response'=>'email or ID not found'
+    ], JSON_PRETTY_PRINT);
+    exit();
+}
 exit();
 
 ?>
--- src/Query/Login.php
+++ src/Query/Login.php
@@ -8,55 +8,21 @@ use NewdichAuth\Authentication;
 
 class Login{
     private $dto;
-    private $mid;
     private $table = Platform::ADMINS_TABLE;
 
-    public function __construct(AnsofraDto $dto, Index $mid){
+    public function __construct(AnsofraDto $dto){
         $this->dto=$dto;
-        $this->mid=$mid;
     }
 
     public function process(){
         $where = [
-            'email'=>$this->mid->cleanData($this->dto->email),
-            'ID'=>$this->mid->cleanData($this->dto->ID)
+            'email'=>$this->dto->email,
+            'ID'=>$this->dto->ID,
         ];
 
         $newMIg = new Migration(null, $this->table);
         $mig = $newMIg->get($where, 0, 1);
-        $decodeMig = json_decode($mig, true);
-        if($decodeMig['status']==='success'){
-            $response = $decodeMig['response'][0];
-            $password = $response['password'];
-            $pwd = $this->mid->cleanData($this->dto->password);
-            if($pwd === $password){
-                $email = $this->mid->cleanData($this->dto->email);
-                $role = "role";
-                $newAuth  = new Authentication();
-                $auth = $newAuth->auth($email, $role);
-                $decodeAuth = json_decode($auth, true);
-                if($decodeAuth['status']==='success'){
-                    $res = $decodeAuth['response'];
-                    $response["response"] = $res;
-                    return json_encode([
-                        'status'=>'success',
-                        'response'=>$response
-                    ], JSON_PRETTY_PRINT);
-                }
-                else{
-                    return $auth;
-                }
-            }
-            else{
-                return json_encode([
-                    'status'=>'failed',
-                    'response'=>'incorrect password'
-                ], JSON_PRETTY_PRINT);
-            }
-        }
-        else{
-            return $mig;
-        } 
+        return $mig;
     }
 }
 
