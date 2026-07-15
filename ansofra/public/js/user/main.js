3c03662ba19356c2cbb45c48c07260b62def395d
shola129 <ekundayoshola129@gmail.com>
2026-07-15 23:34:44 +0100

    SaaS project

--- ansofra/public/js/user/main.js
+++ ansofra/public/js/user/main.js
@@ -139,7 +139,6 @@ async function fetchSubjectList(department, org_code) {
 
 // }
 
-
 /*    FETCH — exam details*/
 async function fetchExamDetails(department, org_code) {
   const btn    = document.getElementById('start-btn');
